68dd5b5fdd8376cbdafb22b64bee0f216f7ee341
Allen Kim <allenhwkim@gmail.com>
2022-06-27 11:41:39 -0400

    updated tests

--- test/sinon-stub/moduleUnderTest.test.js
+++ test/sinon-stub/moduleUnderTest.test.js
@@ -1,15 +1,14 @@
-const assert = require("assert");
 const sinon = require("sinon");
+const expect = require('chai').expect;
 
 const dependencyModule = require("./dependencyModule");
 const { getTheSecret } = require("./moduleUnderTest");
 
 describe("moduleUnderTest", function () {
   describe("when the secret is 3", function () {
-    it("should be returned with a string prefix", function () {
+    it('should be returned with a string prefix', function () {
       sinon.stub(dependencyModule, "getSecretNumber").returns(3);
-      const result = getTheSecret();
-      assert.equal(result, "The secret was: 3");
+      expect(getTheSecret()).to.equal("The secret was: 3");
     });
   });
 });
--- test/sinon-stub/userUtils.test.js
+++ test/sinon-stub/userUtils.test.js
@@ -1,72 +1,51 @@
-const assert = require("assert");
 const sinon = require("sinon");
+const expect = require("chai").expect;
+
 const userUtils = require("./userUtils");
-const userApi = require("./userApi");
+const uApi = require("./userApi");
 
 function aUser(id) {
-  return {
-    id,
-    email: `someemail@user${id}.com`,
-    first_name: `firstName${id}`,
-    last_name: `lastName${id}`,
-    avatar: `https://www.somepage${id}.com`,
-  };
+  return {id, first_name: `firstName${id}`, last_name: `lastName${id}` };
 }
 
 describe("userUtils", function () {
   let getPageOfUsersStub;
 
-  beforeEach(function () {
-    getPageOfUsersStub = sinon.stub(userApi, "getPageOfUsers");
-  });
+  // beforeEach(() => getPageOfUsersStub = sinon.stub(require('./userApi'), 'getPageOfUsers'))
+  beforeEach(() => getPageOfUsersStub = sinon.stub(uApi, 'getPageOfUsers'))
+  afterEach(() => getPageOfUsersStub.restore())
 
-  afterEach(function () {
-    getPageOfUsersStub.restore();
-  });
+  describe('when a single page of users exists', function() {
+    it('should return users from that page', async function() {
+      // mock
+      getPageOfUsersStub.returns(Promise.resolve({
+        page: 1, total_pages: 1, data: [aUser(1), aUser(2), aUser(3)]
+      }))
 
-  describe("when a single page of users exists", function () {
-    it("should return users from that page", async function () {
-      // Arrange
-      const pageOfUsers = {
-        page: 1,
-        total_pages: 1,
-        data: [aUser(1), aUser(2), aUser(3)],
-      };
-
-      getPageOfUsersStub.returns(Promise.resolve(pageOfUsers));
-
-      // Act
+      // run
       const result = await userUtils.getAllUsers();
 
-      // Assert
-      assert.equal(result.length, 3);
-      assert.equal(getPageOfUsersStub.calledOnce, true);
-    });
-  });
+      // verify
+      expect(result.length).to.equal(3);
+      expect(getPageOfUsersStub.calledOnce).to.equal(true);
+    })
+  })
 
-  describe("when multiple pages of users exists", function () {
-    it("should return a combined list of all users", async function () {
-      // Arrange
-      const pageOfUsers1 = {
-        page: 1,
-        total_pages: 2,
-        data: [aUser(1), aUser(2), aUser(3)],
-      };
-      const pageOfUsers2 = {
-        page: 2,
-        total_pages: 2,
-        data: [aUser(4), aUser(5)],
-      };
+  describe('when multiple pages of users exists', function() {
+    it('should return users by page', async function() {
+      // set
+      const page1Users = {page: 1, total_pages: 2, data: [aUser(1), aUser(2), aUser(3)]};
+      const page2Users = {page: 2, total_pages: 2, data: [aUser(4), aUser(5)]};
+      getPageOfUsersStub.withArgs(1).returns(Promise.resolve(page1Users));
+      getPageOfUsersStub.withArgs(2).returns(Promise.resolve(page2Users));
 
-      getPageOfUsersStub.withArgs(1).returns(Promise.resolve(pageOfUsers1));
-      getPageOfUsersStub.withArgs(2).returns(Promise.resolve(pageOfUsers2));
-
-      // Act
+      // run
       const result = await userUtils.getAllUsers();
 
-      // Assert
-      assert.equal(result.length, 5);
-      assert.equal(getPageOfUsersStub.callCount, 2);
-    });
+      // verify
+      expect(result.length).to.equal(5);
+      expect(getPageOfUsersStub.callCount).to.equal(2);
+    })
   });
+
 });
